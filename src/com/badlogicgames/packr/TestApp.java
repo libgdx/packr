64d5a42cc412d895b9a9fcb6d72e3d8920b2fd18
badlogic <contact@badlogicgames.com>
2014-04-25 17:07:02 +0200

    simple test app for testing

---
+++ src/com/badlogicgames/packr/TestApp.java
@@ -0,0 +1,13 @@
+
+package com.badlogicgames.packr;
+
+import javax.swing.JFrame;
+
+public class TestApp {
+	public static void main (String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("My Test App");
+		frame.setSize(480, 320);
+		frame.setVisible(true);
+	}
+}
