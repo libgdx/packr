6f00c9ae30b70489a9ce7b6f84c0a34a5cbe6d46
Daniel Ludwig <codi@code-disaster.com>
2015-11-18 21:46:08 +0100

    Adjusted Linux build script to compile 32 & 64 bit versions.

--- src/main/native/packr/build-linux.sh
+++ src/main/native/packr/build-linux.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
-./fips set config linux-make-release
-./fips clean && ./fips build
 
 if [ `getconf LONG_BIT` = "64" ]
 then
+
+    echo "Building 64 bit executable"
+    ./fips set config linux-make-release
+    ./fips clean && ./fips build
+
     echo "Copying 64 bit executable ..."
     cp ../fips-deploy/packr-native/linux-make-release/packr ../../resources/packr-linux-x64
+
+    echo "Building 32 bit executable"
+    ./fips set config packr-linux32-make-release
+    ./fips clean && ./fips build
+
+    echo "Copying 32 bit executable ..."
+    cp ../fips-deploy/packr-native/packr-linux32-make-release/packr ../../resources/packr-linux
+
 else
+
+    echo "Building 32 bit executable"
+    ./fips set config linux-make-release
+    ./fips clean && ./fips build
+
     echo "Copying 32 bit executable ..."
     cp ../fips-deploy/packr-native/linux-make-release/packr ../../resources/packr-linux
+
 fi
