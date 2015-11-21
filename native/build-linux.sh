f9b8d5132f81a563553577bd0ee582a23f00ae5e
Daniel Ludwig <codi@code-disaster.com>
2015-11-21 10:17:25 +0100

    Deleted deprecated native code.

--- native/build-linux.sh
+++
@@ -1,7 +0,0 @@
-premake4 --file=premake4-linux.lua gmake
-make clean config=release32
-make config=release32
-mv packr ../src/main/resources/packr-linux
-make clean config=release64
-make config=release64
-mv packr ../src/main/resources/packr-linux-x64
