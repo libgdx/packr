c28cc4fe45108b4a1cca9a6c7928ab6c812775a3
Cuong Truong <tduccuong@gmail.com>
2018-02-13 17:03:43 +0100

    fix: cannot create an existing folder in linux; copy copyDirectory does not follow symlink in linux

--- src/main/java/com/badlogicgames/packr/PackrFileUtils.java
+++ src/main/java/com/badlogicgames/packr/PackrFileUtils.java
@@ -28,65 +28,65 @@ import java.nio.file.attribute.BasicFileAttributes;
  */
 class PackrFileUtils {
 
-	static void mkdirs(File path) throws IOException {
-		if (!path.mkdirs()) {
-			throw new IOException("Can't create folder(s): " + path);
-		}
-	}
+  static void mkdirs(File path) throws IOException {
+    if (!path.mkdirs()) {
+      throw new IOException("Can't create folder(s): " + path);
+    }
+  }
 
-	static void chmodX(File path) {
-		if (!path.setExecutable(true)) {
-			System.err.println("Warning! Failed setting executable flag for: " + path);
-		}
-	}
+  static void chmodX(File path) {
+    if (!path.setExecutable(true)) {
+      System.err.println("Warning! Failed setting executable flag for: " + path);
+    }
+  }
 
-	static void delete(File path) throws IOException {
-		if (!path.delete()) {
-			throw new IOException("Can't delete file or folder: " + path);
-		}
-	}
+  static void delete(File path) throws IOException {
+    if (!path.delete()) {
+      throw new IOException("Can't delete file or folder: " + path);
+    }
+  }
 
-	/**
-	 * Copies directories, preserving file attributes.
-	 *
-	 * The {@link org.zeroturnaround.zip.commons.FileUtilsV2_2#copyDirectory(File, File)} function does not
-	 * preserve file attributes.
-	 */
-	static void copyDirectory(File sourceDirectory, File targetDirectory) throws IOException {
+  /**
+   * Copies directories, preserving file attributes.
+   *
+   * The {@link org.zeroturnaround.zip.commons.FileUtilsV2_2#copyDirectory(File, File)} function does not
+   * preserve file attributes.
+   */
+  static void copyDirectory(File sourceDirectory, File targetDirectory) throws IOException {
 
-		final Path sourcePath = Paths.get(sourceDirectory.toURI());
-		final Path targetPath = Paths.get(targetDirectory.toURI());
+    final Path sourcePath = Paths.get(sourceDirectory.toURI()).toRealPath();
+    final Path targetPath = Paths.get(targetDirectory.toURI());
 
-		Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-				Path relative = sourcePath.relativize(dir);
-				Path target = targetPath.resolve(relative);
-				File folder = target.toFile();
-				if (!folder.exists()) {
-					mkdirs(folder);
-				}
-				return FileVisitResult.CONTINUE;
-			}
+    Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path relative = sourcePath.relativize(dir);
+        Path target = targetPath.resolve(relative);
+        File folder = target.toFile();
+        if (!folder.exists()) {
+          mkdirs(folder);
+        }
+        return FileVisitResult.CONTINUE;
+      }
 
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				Path relative = sourcePath.relativize(file);
-				Path target = targetPath.resolve(relative);
-				Files.copy(file, target, StandardCopyOption.COPY_ATTRIBUTES);
-				return FileVisitResult.CONTINUE;
-			}
-		});
-	}
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path relative = sourcePath.relativize(file);
+        Path target = targetPath.resolve(relative);
+        Files.copy(file, target, StandardCopyOption.COPY_ATTRIBUTES);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
 
-	static void deleteDirectory(File directory) throws IOException {
-		FileUtilsV2_2.deleteDirectory(directory);
-	}
+  static void deleteDirectory(File directory) throws IOException {
+    FileUtilsV2_2.deleteDirectory(directory);
+  }
 
-	static void copyFile(File source, File target) throws IOException {
-		Path sourcePath = Paths.get(source.toURI());
-		Path targetPath = Paths.get(target.toURI());
-		Files.copy(sourcePath, targetPath, StandardCopyOption.COPY_ATTRIBUTES);
-	}
+  static void copyFile(File source, File target) throws IOException {
+    Path sourcePath = Paths.get(source.toURI());
+    Path targetPath = Paths.get(target.toURI());
+    Files.copy(sourcePath, targetPath, StandardCopyOption.COPY_ATTRIBUTES);
+  }
 
 }
