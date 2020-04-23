f259671749d9c2c0ffe95430b6f1d9ac62337fb5
Karl Sabo <karl@nimblygames.com>
2020-04-22 22:39:17 -0600

    Change version to 2.3.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Nimbly Games, LLC
+ * Copyright 2020 See AUTHORS file
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 
 group = "com.nimblygames.packr"
-version = "2.2.0"
+version = "2.3.0-SNAPSHOT"
 
 plugins {
    base
