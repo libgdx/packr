e02dd97547e8507e275c9a77541b46493afe874b
Karl Sabo <karl@nimblygames.com>
2021-03-02 22:12:41 -0700

    Remove debug code, add default configuration path case

--- PackrLauncher/src/main/cpp/packr.cpp
+++ PackrLauncher/src/main/cpp/packr.cpp
@@ -466,7 +466,7 @@ bool setCmdLineArguments(int argc, dropt_char **argv) {
                 }
                 else {
                     if (verbose) {
-                        cout << "Using default configuration file " << defaultConfigurationPath << " ..." << executableName << endl;
+                        cout << "Using default configuration file " << defaultConfigurationPath << " ..." << endl;
                     }
                     configurationPath = defaultConfigurationPath;
                 }
@@ -475,7 +475,7 @@ bool setCmdLineArguments(int argc, dropt_char **argv) {
             // treat all arguments as "remains"
             remains = &argv[1];
             if (verbose) {
-                cout << "Using default configuration file " << defaultConfigurationPath << " ..." << executableName << endl;
+                cout << "Using default configuration file " << defaultConfigurationPath << " ..." << endl;
             }
             configurationPath = defaultConfigurationPath;
         }
@@ -501,7 +501,9 @@ bool setCmdLineArguments(int argc, dropt_char **argv) {
             cmdLineArgc++;
             remains++;
         }
-    }
+    } else {
+		configurationPath = defaultConfigurationPath;
+	}
 
     dropt_free_context(droptContext);
 
